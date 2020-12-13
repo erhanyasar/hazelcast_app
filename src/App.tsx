cb8647479fc8750ad633dc5524160b5ff9442dec
Erhan Yaşar <mail@erhanyasar.com.tr>
2020-12-13 21:55:37 +0300

    codegen, button and it's css implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Please click the button below to proceed.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="App-button"
         >
-          Learn React
-        </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sign in with GitHub
+          </a>
+        </button>
       </header>
     </div>
   );
